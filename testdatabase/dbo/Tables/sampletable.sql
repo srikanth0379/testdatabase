3371c5fcd79d581b95391537802c8f36639f423b
srikanth0379 <srikanth0379@gmail.com>
2020-09-02 10:08:38 +0000

    new column added "age"

--- testdatabase/dbo/Tables/sampletable.sql
+++ testdatabase/dbo/Tables/sampletable.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[sampletable]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [name] VARCHAR(50) NULL, 
+    [name] VARCHAR(50) NOT NULL, 
+    [age] VARCHAR(50) NOT NULL,
     [key] VARCHAR(50) NULL
 )
